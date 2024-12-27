f818ad285285f03442a1b2cbfeb3719df8c24a7d
Rhitottam De <rhitottam@rendernet.ai>
2024-12-27 18:36:00 +0530

    fix: add tests for load testing canvas

--- tests/feed-performance-tests.spec.js
+++ tests/feed-performance-tests.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, beforeEach, expect } = require('@playwright/test');
+const { test, beforeEach, beforeAll } = require('@playwright/test');
 const dotenv = require('dotenv');
 const feedData = require('../test-data/canvasData.json');
 
@@ -352,15 +352,6 @@ const mockAPis = async (page) => {
     console.log('normal');
     await route.abort();
   });
-  // await page.route('*/**/app/v1/media/studio/feed?page=2&page_size=20',  async route => {
-  //   const json = { data: [], err: {}};
-  //   await route.fulfill({ json });
-  // });
-  // await page.route(/.*\/app\/v1\/media\/studio\/feed\.*/, async route => {
-  //   const json = feedData;
-  //   console.log('absolute');
-  //   await route.fulfill({ json });
-  // });
 }
 
 const addFPSCounter = async (page) => {
@@ -436,6 +427,7 @@ const NavigationTypes = {
 }
 
 const navigateToStudioFeed = async (p) => {
+  await p.waitForSelector('#navbar-container');
   try {
     await p.waitForSelector('a[href="/app/text-to-image"]', {
       timeout: 1000,
@@ -449,6 +441,25 @@ const navigateToStudioFeed = async (p) => {
   }
   await p.click('a[href="/app/text-to-image"]');
 }
+
+const navigateToCanvasPage = async (p) => {
+  await p.waitForSelector('#navbar-container');
+  try {
+    await p.waitForSelector('a[href="/app/canvases"]', {
+      timeout: 1000,
+    });
+  }
+  catch (e) {
+    await p.click('#mobile-menu-button');
+    await p.waitForSelector('a[href="/app/canvases"]', {
+      timeout: 1000,
+    });
+  }
+  await p.click('a[href="/app/canvases"]');
+  await p.waitForSelector('#canvas-image-wrapper');
+  await p.click('#canvas-image-wrapper');
+}
+
 
 const getStudioFeedLoadAndScrollReadings = async (p, navigationType, maxScrolls = 200) => {
   const readings = {};
@@ -493,11 +504,134 @@ const getStudioFeedLoadAndScrollReadings = async (p, navigationType, maxScrolls 
   return readings;
 }
 
-test('Load and scroll studio feed media: Initial Load and Reload', async ({ page: p, browserName, isMobile }, testInfo) => {
-  // const context = await mockAPis(browser);
-  // const p = await context.newPage();
-  // await mockAPis(p);
-  console.log(`Running on: ${isMobile ? 'Mobile': 'Desktop'}, Browser: ${browserName === 'webkit' ? 'safari': browserName}`);
+const performZoomIn = async (p, count= 1) => {
+  for (let i=0;i<count;i++) {
+    await p.waitForSelector('#canvas-zoom-in-tool');
+    await p.click('#canvas-zoom-in-tool');
+    await p.waitForTimeout(1000);
+  }
+}
+
+const performZoomOut = async (p, count = 1) => {
+  for (let i=0;i<count;i++) {
+    await p.waitForSelector('#canvas-zoom-out-tool');
+    await p.click('#canvas-zoom-out-tool');
+    await p.waitForTimeout(1000);
+  }
+}
+
+const performBasicCanvasOperations = async (p, start, count=1) => {
+  for (let i=0;i<count;i++) {
+    await performZoomIn(p, 2);
+    await p.mouse.move(start[0], start[1], {steps: 2});
+    await performPan(p, start, [-200, 0]);
+    // await addTimeout(2000);
+    await performPan(p, start, [200, 0]);
+    // await addTimeout(2000);
+    await performPan(p, start, [0, -200]);
+    // await addTimeout(2000);
+    await performPan(p, start, [0, 200]);
+    await performZoomOut(p, 2);
+    await p.mouse.move(start[0], start[1], {steps: 2});
+    await performPan(p, start, [-100, 0]);
+    // await addTimeout(2000);
+    await performPan(p, start, [100, 0]);
+    // await addTimeout(2000);
+    await performPan(p, start, [0, -100]);
+    // await addTimeout(2000);
+    await performPan(p, start, [0, 100]);
+  }
+}
+
+const performPan = async (p, move, diff, count = 1) => {
+  for (let i=0;i<count;i++) {
+    await p.keyboard.down('Space');
+    await p.mouse.down({
+      button: 'left',
+    });
+    await p.mouse.move(move[0] + diff[0], move[1] + diff[1], {steps: 2});
+    await p.mouse.up({
+      button: 'left',
+    });
+    await p.mouse.move(move[0], move[1], {steps: 1});
+    await p.keyboard.up('Space');
+    await p.waitForTimeout(1000);
+  }
+}
+
+const periodicCheckForCanvasImagesCompletion = async (p) => {
+  return await p.evaluate(async () => {
+    return new Promise((resolve) => {
+      let canvasImageShapes, completedCanvasImageShapes;
+      const startLoadTime = performance.now();
+      const checkForImages = setInterval(() => {
+        canvasImageShapes = document.querySelectorAll('div[data-shape-type="canvas-image"]');
+        completedCanvasImageShapes = Array.from(canvasImageShapes).filter((imageShape) => {
+          const images = imageShape.getElementsByTagName('img');
+          return Array.from(images).every((image) => image.src?.length);
+        });
+        if (canvasImageShapes.length === completedCanvasImageShapes.length) {
+          clearInterval(checkForImages);
+
+          resolve(performance.now() - startLoadTime);
+        }
+      }, 1000);
+    });
+  });
+
+}
+
+const getCanvasFeedAndPerformOperations = async (p, navigationType, operationsRepeat= 1) => {
+  const readings = {}
+  if(navigationType === NavigationTypes.INITIAL) {
+    await p.waitForURL(process.env.BASE_URL);
+  }
+  else if(navigationType === NavigationTypes.RELOAD) {
+    await p.goto(process.env.BASE_URL, {timeout: 120000});
+  }
+  await p.waitForTimeout(500);
+  await addFPSCounter(p);
+  await runFPSCounter(p);
+  await navigateToCanvasPage(p);
+  const navigationTimingData = await getNavigationTimingData(p);
+  readings.timeToFirstByte = navigationTimingData[0].responseStart - navigationTimingData[0].fetchStart;
+  readings.timeToInteractive = navigationTimingData[0].domInteractive - navigationTimingData[0].fetchStart;
+  readings.timeToLoadPage = navigationTimingData[0].loadEventEnd - navigationTimingData[0].fetchStart;
+  const paintTimingData = await getPaintTimingData(p);
+  readings.firstPaint = paintTimingData.find((element) => element.name === 'first-paint')?.startTime;
+  readings.firstContentfulPaint = paintTimingData.find((element) => element.name === 'first-contentful-paint')?.startTime;
+  readings.largestContentfulPaint = await getLargestContentfulPaintData(p);
+  readings.blockingTime = await getBlockingTimeData(p);
+  await p.waitForTimeout(10000);
+  await p.waitForSelector('#cross-button');
+  await p.click('#cross-button');
+  const fpsCounterData = await getFPSCounterData(p);
+  readings.averageLoadFPS = fpsCounterData.averageFps;
+  readings.minLoadFPS = fpsCounterData.minFps;
+  readings.maxLoadFPS = fpsCounterData.maxFps;
+  await clearFPSCounter(p);
+  readings.canvasImageLoadTime = await periodicCheckForCanvasImagesCompletion(p);
+  const e = await p.$('.tl-canvas');
+  const box = await e.boundingBox();
+  const point = [box.x + box.width / 2, box.y + box.height / 2];
+  await performBasicCanvasOperations(p, point, operationsRepeat);
+  const fpsCounterDataCanvasOperations = await getFPSCounterData(p);
+  readings.averageCanvasOperationFPS = fpsCounterDataCanvasOperations.averageFps;
+  readings.minCanvasOperationFPS = fpsCounterDataCanvasOperations.minFps;
+  readings.maxCanvasOperationFPS = fpsCounterDataCanvasOperations.maxFps;
+  await clearFPSCounter(p);
+  return readings;
+}
+
+const constructInitialReadingsJson = (isMobile, browserName) => {
+  return {
+    browser: browserName === 'webkit' ? 'safari' : browserName,
+    isMobile,
+    label: `${isMobile ? 'Mobile' : 'Desktop'} - ${browserName === 'webkit' ? 'safari' : browserName}`,
+  };
+}
+
+beforeEach(async ({ page: p }) => {
   await p.goto(process.env.BASE_URL+process.env.LOGIN_PATH, {
     timeout: 120000,
   });
@@ -510,15 +644,39 @@ test('Load and scroll studio feed media: Initial Load and Reload', async ({ page
   await p.click('#login-button');
   await p.waitForURL(process.env.BASE_URL);
   await p.waitForTimeout(500);
+})
 
-
-  const readingsJSON = {};
-
-  readingsJSON[NavigationTypes.INITIAL] = await getStudioFeedLoadAndScrollReadings(p, NavigationTypes.INITIAL, 10);
+test('Load and scroll studio feed media: Initial Load and Reload', async ({ page: p, browserName, isMobile }, testInfo) => {
+  console.log(`Running on: ${isMobile ? 'Mobile': 'Desktop'}, Browser: ${browserName === 'webkit' ? 'safari': browserName}`);
+  const maxNumberOfScrolls = 1000;
+  const readingsJSON = constructInitialReadingsJson(isMobile, browserName);
+  readingsJSON[NavigationTypes.INITIAL] = await getStudioFeedLoadAndScrollReadings(p, NavigationTypes.INITIAL, maxNumberOfScrolls);
   await p.waitForTimeout(1000);
-
-  readingsJSON[NavigationTypes.RELOAD] = await getStudioFeedLoadAndScrollReadings(p, NavigationTypes.RELOAD, 10);
+  readingsJSON[NavigationTypes.RELOAD] = await getStudioFeedLoadAndScrollReadings(p, NavigationTypes.RELOAD, maxNumberOfScrolls);
   console.log(readingsJSON);
-  await testInfo.attach('performance readings', { body: readingsJSON, contentType: 'application/json' });
+  test.info().annotations.push({
+    type: `Studio Feed Load Tests - ${readingsJSON.label}`,
+    description: 'Load and scroll studio feed media: Initial Load and Reload',
+    performanceTestReadings: readingsJSON
+  });
+  await testInfo.attach(`studio-feed-performance-test readings-${readingsJSON.label}`, { body: JSON.stringify(readingsJSON), contentType: 'application/json' });
 });
 
+
+test('Load and perform pan and zoom operations on Canvas: Initial and Reload', async({page: p, browserName, isMobile}, testInfo) => {
+  test.skip(isMobile, 'Canvas testing only for Desktop devices');
+  console.log(`Running on: ${isMobile ? 'Mobile': 'Desktop'}, Browser: ${browserName === 'webkit' ? 'safari': browserName}`);
+
+  const numberOfBasicOperationRepetitions = 2;
+  const readingsJSON = constructInitialReadingsJson(isMobile, browserName);
+  readingsJSON[NavigationTypes.INITIAL] = await getCanvasFeedAndPerformOperations(p, NavigationTypes.INITIAL, numberOfBasicOperationRepetitions);
+  await p.waitForTimeout(1000);
+  readingsJSON[NavigationTypes.RELOAD] = await getCanvasFeedAndPerformOperations(p, NavigationTypes.RELOAD, numberOfBasicOperationRepetitions);
+  console.log(readingsJSON);
+  test.info().annotations.push({
+    type: `Infinite Canvas Feed Load Tests - ${readingsJSON.label}`,
+    description: 'Load and perform pan and zoom operations on Canvas: Initial and Reload',
+    performanceTestReadings: readingsJSON
+  });
+  await testInfo.attach(`canvas-feed-performance-test-readings-${readingsJSON.label}`, { body: JSON.stringify(readingsJSON), contentType: 'application/json' });
+});
